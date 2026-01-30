95230b0bc2de4a539ca9ccc9058de8b784efb2d3
Ifesinachi Samrose Eke <ekesamrose@gmail.com>
2026-01-30 14:31:02 +0100

    feat: implement dynamic testimonials section with styling and responsive design

--- scripts/testimonial.js
+++ scripts/testimonial.js
@@ -3,17 +3,17 @@ import { createShowcaseFooter } from "../components/footerShowcase.js";
 
 createShowcaseFooter("footer-showcase-container");
 
-// const container = document.getElementById("testimonials");
+const container = document.getElementById("testimonials");
 
-// container.innerHTML = testimonialsData
-//   .map(
-//     ({ name, track, avatar, quote }) => `
-//     <article class="testimonial-card">
-//       <img src="${avatar}" alt="${name}" />
-//       <h3>${name}</h3>
-//       <p class="track">${track} Track</p>
-//       <p class="quote">“${quote}”</p>
-//     </article>
-//   `,
-//   )
-//   .join("");
+container.innerHTML = testimonialsData
+  .map(
+    ({ name, track, avatar, quote }) => `
+    <article class="testimonial-card">
+      <img src="${avatar}" alt="${name}" />
+      <h3>${name}</h3>
+      <p class="track">${track} Track</p>
+      <p class="quote">“${quote}”</p>
+    </article>
+  `,
+  )
+  .join("");
